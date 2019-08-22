4bda1c8cc8cef2d7658533de4f95686987f4f634
rashifmalikilyasa <malikutama30@gmail.com>
2019-08-22 10:36:11 +0700

    test2

--- app/src/main/java/com/example/fragmentmi/MainActivity.java
+++ app/src/main/java/com/example/fragmentmi/MainActivity.java
@@ -1,52 +1,111 @@
 package com.example.fragmentmi;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements
-        BottomNavigationView.OnNavigationItemSelectedListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private List<Song> songList;
+    private SongAdapter songAdapter;
+    String[] names = {"I Took A Pill In Ibiza", "7 Years", "Pillow Talk", "Work From Home", "Never Forget You", "Don't Let Me Down",
+            "Love Yourself", "Me, Myself & I", "Cake By The Ocean", "Dangerous Woman", "My House", "Stressed Out", "One Dance", "Middle", "No"};
+    String[] singers = {"Mike Posner", "Lukas Graham", "Zayn", "Fifth Harmony",
+            "Zara Larsson & MNEK", "The Chainsmokers",
+            "Justin Bieber", "G-Eazy x Bebe Rexha", "DNCE", "Ariana Grande", "FloRida",
+            "Twenty one Pilots", "Drake", "DJ Snake", "Meghan Trainer"};
+    int[] pics = {
+            R.drawable.took_a_pill,
+            R.drawable.seven_years,
+            R.drawable.pillow_talk,
+            R.drawable.work,
+            R.drawable.never_forget_you,
+            R.drawable.dont_let_me_down,
+            R.drawable.love_yourself,
+            R.drawable.me_myself_and_i,
+            R.drawable.cake_by_the_ocean,
+            R.drawable.dangerous_woman,
+            R.drawable.my_house_florida,
+            R.drawable.stressed_out,
+            R.drawable.one_dance,
+            R.drawable.middle,
+            R.drawable.no};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+//setting untuk improve perfomance
+//content tdk dapat mengubah size layout recyclerview
+        if (mRecyclerView != null) {
+            mRecyclerView.setHasFixedSize(true);
+        }
+// //using a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+//intializing an arraylist called songlist
+        songList = new ArrayList<>();
+//add song list
+        for (int i = 0; i < names.length; i++) {
 
-        loadFragment(new HomeFragment());
-        BottomNavigationView navView = findViewById(R.id.navigation);
-        navView.setOnNavigationItemSelectedListener(this);
+            Song song = new Song(names[i], singers[i], i + 1, pics[i]);
+            songList.add(song);
+        }
+//initializing adapter
+        songAdapter = new SongAdapter(songList);
+//specifying an adapter to access data, create views and replace the content
+        mRecyclerView.setAdapter(songAdapter);
+        songAdapter.notifyDataSetChanged();
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(MainActivity.this, "Card at " + position + " is clicked", Toast.LENGTH_SHORT).show();
+                    }
+                }));
     }
-
-    Fragment fragment = null;
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    fragment = new HomeFragment();
-                    break;
-                case R.id.navigation_dashboard:
-                    fragment = new DashboardFragment();
-                    break;
-                case R.id.navigation_notifications:
-                    fragment = new NotificationFragment();
-                    break;
-                case R.id.navigation_profile:
-                    fragment = new ProfileFragment();
-            }
-            return loadFragment(fragment);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
-    private boolean loadFragment(Fragment fragment){
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, fragment)
-                    .commit();
-            return true;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_grid:
+// use this in case of gridlayoutmanager
+                mLayoutManager = new GridLayoutManager(this, 2);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                break;
+            case R.id.item_staggered_grid:
+// use this in case of Staggered GridLayoutManager
+                mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                break;
+            case R.id.item_horizontal:
+//horizontal linear layout
+                mLayoutManager = new
+                        LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                break;
         }
-        return false;
+        return super.onOptionsItemSelected(item);
     }
 }
